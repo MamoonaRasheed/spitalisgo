6afc4ae9cf07c62d1d080c8e239e39c4623c0ebc
muhammadahsn <30300096+muhammadahsn@users.noreply.github.com>
2025-04-18 12:01:29 +0500

    add loader

--- src/components/middleware/AuthGuard.tsx
+++ src/components/middleware/AuthGuard.tsx
@@ -26,7 +26,11 @@ export default function AuthGuard({
   }, [profile, loading, router]);
 
   if (loading || isAuthorized === null) {
-    return <SignInForm />; // Or a loading spinner
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    )
   }
 
   return <>{children}</>;
