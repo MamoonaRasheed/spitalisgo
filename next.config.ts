45c56b47d85afba0245fdbe833ee4a13ad1b4065
muhammadahsn <30300096+muhammadahsn@users.noreply.github.com>
2025-05-30 01:10:00 +0500

    testing config issue

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,14 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/api/:path*', // Laravel API
+      },
+    ];
+  },
 };
 
 export default nextConfig;
